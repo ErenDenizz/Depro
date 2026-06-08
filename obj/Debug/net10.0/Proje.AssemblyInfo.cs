73208930f2a25d2541ee4129b0f0e19133a0afe5
Eren Deniz Kılıç <eren.kilic353@gmail.com>
2026-06-08 21:05:31 +0300

    text edit

--- obj/Debug/net10.0/Proje.AssemblyInfo.cs
+++ obj/Debug/net10.0/Proje.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proje")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a018f4fa79d315b0d595e2c36af64dd77f191bfe")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proje")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proje")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
